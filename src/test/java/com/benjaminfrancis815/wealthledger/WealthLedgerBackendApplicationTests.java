d5d1b53b917e0f2493bc39e775c5b309ba552539
Benjamin Francis <benjaminfrancis815@gmail.com>
2026-05-04 08:33:22 +0530

    test: disable all tests

--- src/test/java/com/benjaminfrancis815/wealthledger/WealthLedgerBackendApplicationTests.java
+++ src/test/java/com/benjaminfrancis815/wealthledger/WealthLedgerBackendApplicationTests.java
@@ -1,9 +1,11 @@
 package com.benjaminfrancis815.wealthledger;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class WealthLedgerBackendApplicationTests {
 
 	@Test
